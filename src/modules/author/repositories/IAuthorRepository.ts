117bbf4b8c621e5eed969ba7545d6db0800777a8
caio1902araujo <caio2002araujo@gmail.com>
2022-05-17 15:07:20 -0300

    adding interface to author entity repository

---
+++ src/modules/author/repositories/IAuthorRepository.ts
@@ -0,0 +1,14 @@
+import Author from '../infra/typeorm/entities/Author';
+import ICreateAuthorDTO from '../dtos/ICreateAuthorDTO';
+import IFindAllAuthorDTO from '../dtos/IFindAllAuthorDTO';
+
+interface IAuthorRepository{
+  findAllAuthors(data: IFindAllAuthorDTO): Promise<Author[]>;
+	findById(id: string): Promise<Author | undefined>;
+	findByUsername(username: string): Promise<Author | undefined>;
+  delete(id: string): Promise<void>;
+	create(data: ICreateAuthorDTO): Promise<Author>;
+	save(author: Author): Promise<Author>;
+}
+
+export default IAuthorRepository;

f9705365849911e67e1e1bd1ac6c9c05d4b39fcb
caio1902araujo <caio2002araujo@gmail.com>
2022-05-17 15:17:41 -0300

    implementing repository for Author entity

---
+++ src/modules/author/infra/typeorm/repositories/AuthorRepository.ts
@@ -0,0 +1,63 @@
+import { getRepository, Repository, ILike } from 'typeorm';
+
+import Author from '../entities/Author';
+
+import IAuthorRepository from '@modules/author/repositories/IAuthorRepository';
+import ICreateAuthorDTO from '@modules/author/dtos/ICreateAuthorDTO';
+import IFindAllAuthorDTO from '@modules/author/dtos/IFindAllAuthorDTO';
+
+class AuthorRepository implements IAuthorRepository{
+	private ormRepository: Repository<Author>;
+
+	constructor(){
+		this.ormRepository = getRepository(Author);
+	}
+
+  public async delete(id:string) {
+    await this.ormRepository.delete(id);
+  }
+
+  public async findAllAuthors({name='', username='', offset=0, limit=10, order='DESC'}: IFindAllAuthorDTO): Promise<Author[]> {
+    const authors = await this.ormRepository.find({
+      where: {
+        name: ILike('%' + name + '%'),
+        username: ILike('%' + username + '%'),
+      },
+      order: { name: order },
+      take: limit,
+      skip: offset,
+    });
+
+    return authors;
+  }
+
+	public async findById(id: string): Promise<Author | undefined> {
+		const findAuthor = await this.ormRepository.findOne({
+			where: { id },
+		});
+
+		return findAuthor;
+	}
+
+  public async findByUsername(username: string): Promise<Author | undefined> {
+		const findAuthor = await this.ormRepository.findOne({
+			where: { username },
+		});
+
+		return findAuthor;
+	}
+
+  public async create(authorData: ICreateAuthorDTO): Promise<Author>{
+    const author = this.ormRepository.create(authorData);
+
+    await this.ormRepository.save(author);
+
+    return author
+  }
+
+  public async save(author: Author): Promise<Author>{
+    return await this.ormRepository.save(author);
+  }
+}
+
+export default AuthorRepository;

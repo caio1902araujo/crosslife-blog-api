b6f029190904dcb4dc7d11c65080426e9e39a439
caio1902araujo <caio2002araujo@gmail.com>
2022-05-17 16:14:00 -0300

    implementing service to display author profile data

---
+++ src/modules/author/services/ShowAuthorProfileService.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from 'tsyringe';
+
+import Author from '../infra/typeorm/entities/Author';
+
+import IAuthorRepository from '../repositories/IAuthorRepository';
+
+import AppError from '@shared/errors/AppError';
+
+@injectable()
+class ShowAuthorProfileService{
+	constructor(
+		@inject('AuthorRepository')
+		private authorRepository: IAuthorRepository,
+	){}
+
+	public async execute(authorId: string): Promise<Author> {
+		const author = await this.authorRepository.findById(authorId);
+
+		if(!author){
+			throw new AppError('Autor(a) não encontrado(a).');
+		}
+
+		return author;
+	}
+}
+
+export default ShowAuthorProfileService;

e7d76817341577effc1019c6c68e3eb81f0384bb
caio1902araujo <caio2002araujo@gmail.com>
2022-05-18 12:03:09 -0300

    creating controller for author management

---
+++ src/modules/author/infra/http/controllers/AuthorController.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+import { instanceToInstance } from 'class-transformer'
+import { container } from 'tsyringe';
+
+import ShowAuthorPresentationService from '@modules/author/services/ShowAuthorPresentationService';
+import CreateAuthorService from '@modules/author/services/CreateAuthorService';
+import DeleteAuthorService from '@modules/author/services/DeleteAuthorService';
+import ListAuthorsService from '@modules/author/services/ListAuthorsService';
+import IFindAllAuthorDTO from '@modules/author/dtos/IFindAllAuthorDTO';
+
+class AuthorController{
+	public async show(request: Request, response: Response): Promise<Response>{
+		const username = request.params.username;
+
+		const showAuthor = container.resolve(ShowAuthorPresentationService);
+		const author = await showAuthor.execute(username);
+
+		return response.json(instanceToInstance(author));
+	}
+
+  public async delete(request: Request, response: Response): Promise<Response>{
+    const id = request.params.id as string;
+    const deleteAuthorService = container.resolve(DeleteAuthorService);
+    await deleteAuthorService.execute(id);
+
+    return response.status(204).json();
+  }
+
+  public async create(request: Request, response: Response): Promise<Response>{
+		const { name, username, password, description } = request.body;
+
+		const createAuthorProfile = container.resolve(CreateAuthorService);
+		const author = await createAuthorProfile.execute({ name, username, password, description });
+
+		return response.json(instanceToInstance(author));
+	}
+
+  public async index(request: Request, response: Response): Promise<Response>{
+    const {name, username, order} = request.query;
+    const limit = parseInt(request.query.limit as string);
+    const offset = parseInt(request.query.offset as string);
+
+		const listAuthorsService = container.resolve(ListAuthorsService);
+		const authors = await listAuthorsService.execute({ name, username, limit, offset, order} as IFindAllAuthorDTO);
+
+		return response.json(instanceToInstance(authors));
+	}
+}
+
+export default AuthorController;

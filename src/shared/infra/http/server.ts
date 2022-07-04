e93a6f4034f3c614284f749fc1c93688602c565f
caio1902araujo <caio2002araujo@gmail.com>
2022-07-04 16:23:14 -0300

    fixing indentation

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -20,20 +20,22 @@ app.use(routes);
 
 routes.use(errors());
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction)=> {
-  if(err instanceof AppError){
-    return response.status(err.statusCode).json({
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'error',
+        message: err.message,
+      });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
       status: 'error',
-			message: err.message,
+      message: 'Internal server error',
     });
-  }
-
-  console.error(err);
-
-  return response.status(500).json({
-		status: 'error',
-		message: 'Internal server error',
-	});
-})
+  },
+);
 
 app.listen(3333, () => console.log('Server started'));
